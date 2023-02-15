a11d80d07df6232057f0339f32b23219961d838c
Guizica <90075123+Guizica@users.noreply.github.com>
2023-02-15 20:34:24 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame, sys
+from settings import *
+
+class Game():
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_HEIGHT,SCREEN_WIDTH))
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            pygame.display.update()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
+    
+    
+
+
